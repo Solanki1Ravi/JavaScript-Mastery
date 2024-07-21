ada18a70fbc37e6b92002b24f40e7e5a73d86a1b
RAVINDAR SINGH <rajnesh42231@gmail.com>
2024-07-21 13:00:06 +0000

    comparisons of null and undefined

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,33 @@
+
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2<1);
+
+let numa = null;
+
+let realNum = Number(numa);
+// console.log(realNum);
+
+
+// console.log(null>0); //false
+// console.log(null>=0); //true
+// console.log(null==0);//false
+// console.log(null<=0);//true but why??
+// console.log(null<0);//false
+
+
+
+let a = undefined;
+let b = Number(a);
+console.log(b);
+
+// console.log(undefined==0); //Nan
+// console.log(undefined>=0);//Nan
+// console.log(undefined<=0);//Nan
+// console.log(undefined>0);//Nan
+// console.log(undefined<0);//Nan
+
+
