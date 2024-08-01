b404c7c200c9fe4ea3fad64c8eec96f8bfe002f3
RAVINDAR SINGH <rajnesh42231@gmail.com>
2024-08-01 04:24:35 +0000

    for-of Loop

---
+++ 05_loops/03_for_of.js
@@ -0,0 +1,57 @@
+/*-------------------- for of loop -------------*/
+
+
+const myArr = ["Ravi","Singh","Solanki"];
+
+for (const val of myArr) {
+   // console.log(val);
+    
+}
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+
+
+const map = new Map()
+
+map.set("In","India")
+map.set("Fr","France")
+map.set("Usa","United states of america")
+map.set("Uk","United Kingdom")
+
+
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+
+// for (const [key,val] of map) {
+//     console.log(key+" :- "+val);
+    
+// }
+
+
+const myObj = {
+    game1:"NFS",
+    game2:"PUBG",
+    game3:"COD"
+}
+
+
+// for (const [key,val] of myObj) {
+
+//     console.log(key+" :- "+val);
+    
+// }
+
+
+
+
+
+
