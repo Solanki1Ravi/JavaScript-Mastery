79c78e94b43a02c8ca81aed4e19a30edc77e12d8
RAVINDAR SINGH <rajnesh42231@gmail.com>
2024-08-04 12:33:35 +0000

    Parent and Child

---
+++ 06_DOM/two.js
@@ -0,0 +1,39 @@
+const Parent = document.querySelector(".parent");
+
+
+// console.log(Parent);
+// console.log(Parent.children);
+// console.log(Parent.children[3]);
+// console.log(Parent.children[3].innerHTML);
+
+// for (let i = 0; i < Parent.children.length; i++) {
+//     console.log(Parent.children[i].innerHTML);
+    
+// }
+
+// Parent.children[3].style.color = "orange"
+
+//Accessing first element child
+// console.log(Parent.firstElementChild);
+
+
+
+//Accessing last element child
+
+// console.log(Parent.lastElementChild);
+
+
+const dayOne = document.querySelector(".day")
+// console.log(dayOne);
+
+//child to parent 
+// console.log(dayOne.parentElement);
+
+// console.log(dayOne.nextElementSibling);
+
+console.log("Nodes: ",Parent.childNodes);
+
+
+
+
+
