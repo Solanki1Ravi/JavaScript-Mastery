433f6be98372c5cc77e3bfc4527bd6473d83327b
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-08-09 11:29:22 +0530

    Inheritance in Javascript
    Inheritance in JavaScript

---
+++ 10_OOPS/06_Inheritance.js
@@ -0,0 +1,39 @@
+class user {
+  constructor(username,email,password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  printDetails() {
+    return `UserName: ${this.username}\n"Email: ${this.email}\nPassword: ${this.password}`;
+  }
+
+  passwordEncryption() {
+    return `${this.password}123456`;
+  }
+
+
+}
+
+class User1 extends user {
+  constructor(username, email, password) {
+    super(username,email,password);
+    
+  }
+
+  
+ 
+}
+
+
+
+const MyUser = new User1("Ravindra singh","ravisolanki@gmail.com",456789)
+
+console.log(MyUser.printDetails());
+console.log(MyUser.passwordEncryption());
+
+console.log(MyUser instanceof user);
+console.log(MyUser instanceof User1);
+
+
