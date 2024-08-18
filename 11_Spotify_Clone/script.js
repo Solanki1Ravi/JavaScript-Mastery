eca1cc74ebee390cee86445dbf6d1d3408dac5e6
RavindraSolanki26 <solankiravindra633@gmail.com>
2024-08-18 13:45:10 +0530

    songs added
    songs added on the playlist sectino

--- 11_Spotify_Clone/script.js
+++ 11_Spotify_Clone/script.js
@@ -2,7 +2,7 @@ console.log("spotify clone");
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/Media/songs/");
-//   let a = await fetch("https://www.youtube.com/watch?v=4DfVxVeqk2o");
+  //   let a = await fetch("https://www.youtube.com/watch?v=4DfVxVeqk2o");
   let response = await a.text();
 
   let div = document.createElement("div");
@@ -15,7 +15,7 @@ async function getSongs() {
   for (let index = 1; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
 
@@ -26,20 +26,37 @@ async function mySongs() {
   let songs = await getSongs();
   console.log(songs);
 
-  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
   for (const song of songs) {
-    songUL.innerHTML+=`<li>${song}</li>`
+    songUL.innerHTML =songUL.innerHTML+ `<li>
     
+    <img class="invert" src="img.svg" alt="">
+    <div class="info">
+
+      <div>${song.slice(0, -10).slice(0,13).replaceAll("-", " ")}</div>
+      <div>Ravi Solanki</div>
+    </div>
+    <div class="playNow">
+
+      <span>Play Now</span>
+    </div>
+    <img class="invert" src="play.svg" alt="">
+    
+    
+    
+    
+                          </li>`;
   }
 
   var audio = new Audio(songs[0]);
-//   audio.play();
+    // audio.play();
 
   audio.addEventListener("loadeddata", () => {
     let duration = audio.duration;
     console.log(duration);
   });
 }
-
 
 mySongs();
